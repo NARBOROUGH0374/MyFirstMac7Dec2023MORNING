0abcb774efcb82aafd93ff9e7c3098442206b066
narb <narborough.grahame@icloud.com>
2023-12-07 10:02:29 +0800

    Initial Commit

---
+++ MyFirstMac7Dec2023MORNING/MyFirstMac7Dec2023MORNINGApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstMac7Dec2023MORNINGApp.swift
+//  MyFirstMac7Dec2023MORNING
+//
+//  Created by Grahame Narborough on 7/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstMac7Dec2023MORNINGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
